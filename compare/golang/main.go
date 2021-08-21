c14ba7daa4e8dde07839c872e728bd24862abdf7
tim <tim.hau@hotmail.de>
2021-08-21 18:17:14 +0200

    Take k from command line in golang

--- compare/golang/main.go
+++ compare/golang/main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/timHau/extensor_coding/algorithm"
 )
 
 func main() {
-	res := algorithm.C("../../src/data/out.brunson_revolution_revolution", 4, 0.1)
-	fmt.Println(res)
+
+	args := os.Args
+
+	if len(args) > 1 {
+		k, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+		res := algorithm.C("../../src/data/out.brunson_revolution_revolution", k, 0.1)
+		fmt.Println(res)
+	}
 }
